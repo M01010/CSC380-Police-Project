42da88d73fa505cd7159694ff57e060b2599a231
mishari <misharialbuhairi@gmail.com>
2023-10-19 11:29:51 +0300

    ALERT and bootsrap import

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
 from config import Config
 
+
+
 app = Flask(__name__)
+app.secret_key = 'mm'  # Ensure you have set a secret key
 
 posts = [
     {
@@ -42,13 +45,15 @@ def connect():
 @app.route('/about')
 def about():
     try:
+        raise Exception("sql error ya 7mar")
         connection = connect()
         cursor = connection.cursor(cursor_factory=RealDictCursor)
         cursor.execute('select * from test')
         result_set = cursor.fetchall()
         return render_template('about.html', tests=result_set)
     except Exception as e:
-        return render_template("error.html", error=e)
+        flash(f'Error: {e}', 'danger')#send the alet
+        return render_template('about.html')
 
 
 if __name__ == '__main__':
