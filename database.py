082c23bbfdd0ca3d408d752e19d853adc98ac7bd
M01010 <ihaamoodi@gmail.com>
2023-10-19 20:04:59 +0300

    cleaned code

--- database.py
+++ database.py
@@ -23,3 +23,95 @@ class Database:
         con = sqlite3.connect('police.db')
         con.row_factory = sqlite3.Row
         return con
+
+    class Test:
+        @staticmethod
+        def get_tests():
+            connection = None
+            cursor = None
+            try:
+                connection = Database.connect_sqlite()
+                cursor = connection.cursor()
+                cursor.execute('select * from test')
+                return cursor.fetchall()
+            except Exception as e:
+                raise e
+            finally:
+                try:
+                    cursor.close()
+                    connection.close()
+                except Exception as e:
+                    print(e)
+
+        @staticmethod
+        def get_test(test_id):
+            connection = None
+            cursor = None
+            try:
+                connection = Database.connect_sqlite()
+                cursor = connection.cursor()
+                cursor.execute(f'select * from test where id={test_id}')
+                return cursor.fetchone()
+            except Exception as e:
+                raise e
+            finally:
+                try:
+                    cursor.close()
+                    connection.close()
+                except Exception as e:
+                    print(e)
+
+        @staticmethod
+        def add_test(test):
+            connection = None
+            cursor = None
+            try:
+                connection = Database.connect_sqlite()
+                cursor = connection.cursor()
+                cursor.execute(f'insert into test values ("{test["id"]}", "{test["name"]}")')
+                connection.commit()
+            except Exception as e:
+                raise e
+            finally:
+                try:
+                    cursor.close()
+                    connection.close()
+                except Exception as e:
+                    print(e)
+
+        @staticmethod
+        def edit_test(test_id, test):
+            connection = None
+            cursor = None
+            try:
+                connection = Database.connect_sqlite()
+                cursor = connection.cursor()
+                cursor.execute(f'update test set id ={test["id"]}, name="{test["name"]}" where id={test_id}')
+                connection.commit()
+            except Exception as e:
+                raise e
+            finally:
+                try:
+                    cursor.close()
+                    connection.close()
+                except Exception as e:
+                    print(e)
+
+        @staticmethod
+        def delete_test(test_id):
+            connection = None
+            cursor = None
+            try:
+                connection = Database.connect_sqlite()
+                cursor = connection.cursor()
+                cursor.execute(f'delete from test where id={test_id}')
+                connection.commit()
+                return
+            except Exception as e:
+                raise e
+            finally:
+                try:
+                    cursor.close()
+                    connection.close()
+                except Exception as e:
+                    print(e)
--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from datetime import datetime
 
 from flask import Flask, render_template, flash, redirect, url_for, request
 from psycopg2.extras import RealDictCursor
+
+from tests import tests
 from database import Database
 
 app = Flask(__name__)
 app.secret_key = '123'
+app.register_blueprint(tests)
 
 
 @app.route('/')
@@ -21,107 +24,6 @@ def about_supabase():
     try:
         connection = Database.connect_supabase()
         cursor = connection.cursor(cursor_factory=RealDictCursor)
-        cursor.execute('select * from test')
-        result_set = cursor.fetchall()
-        diff = datetime.now() - start
-        return render_template('about.html', tests=result_set, time=diff.total_seconds())
-    except Exception as e:
-        flash(f'Error: {e}', 'danger')  # send the alert
-        return render_template('about.html')
-    finally:
-        try:
-            cursor.close()
-            connection.close()
-        except Exception as e:
-            print(e)
-
-
-@app.route('/tests/delete/<test_id>')
-def delete_test(test_id):
-    connection = None
-    cursor = None
-    try:
-        connection = Database.connect_sqlite()
-        cursor = connection.cursor()
-        cursor.execute(f'delete from test where id={test_id}')
-        connection.commit()
-        return redirect(url_for('about_sqlite'))
-    except Exception as e:
-        print(e)
-        flash(f'Error: {e}', 'danger')  # send the alert
-        return redirect(url_for('about_sqlite'))
-    finally:
-        try:
-            cursor.close()
-            connection.close()
-        except Exception as e:
-            print(e)
-
-
-@app.route('/tests/add/', methods=['GET', 'POST'])
-def add_test():
-    if request.method == 'POST':
-        test = {
-            'id': request.form['id'],
-            'name': request.form['name']
-        }
-        connection = None
-        cursor = None
-        try:
-            print(test)
-            connection = Database.connect_sqlite()
-            cursor = connection.cursor()
-            cursor.execute(f'insert into test values ("{test["id"]}", "{test["name"]}")')
-            connection.commit()
-            return redirect(url_for('about_sqlite'))
-        except Exception as e:
-            print(e)
-            flash(f'Error: {e}', 'danger')  # send the alert
-            return render_template('add_test.html', test=test)
-        finally:
-            try:
-                cursor.close()
-                connection.close()
-            except Exception as e:
-                print(e)
-
-    return render_template('add_test.html')
-
-@app.route('/tests/edit/<test_id>', methods=['GET', 'POST'])
-def edit_test(test_id):
-    connection = None
-    cursor = None
-    test = None
-    try:
-        connection = Database.connect_sqlite()
-        cursor = connection.cursor()
-        cursor.execute(f'select * from test where id={test_id}')
-        test = cursor.fetchone()
-        if request.method == 'POST':
-            cursor.execute(f'update test set id ={request.form["id"]}, name="{request.form["name"]}" where id={test_id}')
-            connection.commit()
-            return redirect(url_for('about_sqlite'))
-        return render_template('edit_test.html', test=test)
-    except Exception as e:
-        print(e)
-        flash(f'Error: {e}', 'danger')  # send the alert
-        return render_template('edit_test.html', test=test)
-    finally:
-        try:
-            cursor.close()
-            connection.close()
-        except Exception as e:
-            print(e)
-
-
-@app.route('/about-sqlite')
-def about_sqlite():
-    start = datetime.now()
-    connection = None
-    cursor = None
-    try:
-        connection = Database.connect_sqlite()
-        cursor = connection.cursor()
         cursor.execute('select * from test')
         result_set = cursor.fetchall()
         diff = datetime.now() - start
